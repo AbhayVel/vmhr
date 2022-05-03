f0e76df625e0cfc95e30582317ab4c72c2576f68
abhayvel <quickitdotnet@gmail.com>
2022-05-03 20:25:49 +0530

    added Db and Feed

---
+++ HrSystem/HREntity/Feed.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HREntity
+{
+    public class Feed
+    {
+        public int Id { get; set; }
+
+        public string TextData { get; set; }
+
+        public string Heading { get; set; }
+
+        public string ShortNotes { get; set; }
+
+        public int FeedTypeId { get; set; }
+
+        public string Link { get; set; }
+
+
+        public string UserName { get; set; }
+
+        public DateTime FeedDate { get; set; }
+
+    }
+}
