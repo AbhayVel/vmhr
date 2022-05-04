d52a4ee2bf01e63a2b69a04ddfa641380df0fcc0
Manthan <manthan.mulajkar@gmail.com>
2022-05-04 19:42:09 +0530

    Timesheet added

---
+++ HrSystem/HREntity/TimeSheet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HREntity
+{
+    public class TimeSheet
+    {
+        public string TextData { get; set; }
+
+        public string Heading { get; set; }
+
+        public string ShortNotes { get; set; }
+
+        public float TimeSpend { get; set; }
+
+        public DateTime TaskStartDate { get; set; }
+
+        public DateTime TaskEndDate { get; set; }
+
+        public DateTime TaskDate { get; set; }
+    }
+}
