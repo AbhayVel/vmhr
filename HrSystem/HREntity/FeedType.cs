30e5b20a65462df9844373b8a38c4c57bfcbba9d
ketaki2893 <ketakipogade@gmail.com>
2022-05-04 19:38:10 +0530

    added feedType

---
+++ HrSystem/HREntity/FeedType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HREntity
+{
+    public class FeedType
+    {
+        public int Id { get; set; }
+
+        public string TypeText { get; set; }
+    }
+}
