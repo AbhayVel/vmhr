99b5419cc5c6d676d34f328966b243c7bbf301ec
ketaki2893 <ketakipogade@gmail.com>
2022-05-12 19:29:01 +0530

    sorted serch

--- HrSystem/HrSystem/Controllers/FeedController.cs
+++ HrSystem/HrSystem/Controllers/FeedController.cs
@@ -1,48 +1,131 @@
 ﻿using HREntity;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HrSystem.Controllers
 {
     public class FeedController : Controller
     {
-        public IActionResult Index()
-        {
+        public List<Feed> GetFeed() {
             List<Feed> feed = new List<Feed>();
 
             feed.Add(new Feed
             {
                 Id = 1,
-                TextData = "Heading"
+                TextData = "Python Learning ",
+                Heading = "Python",
+                ShortNotes = "Python Learning Session",
+                FeedTypeId = 1,
+                Link = "#",
+                UserName = "Aditi"
+
             });
 
             feed.Add(new Feed
             {
                 Id = 2,
-                TextData = "ShortNotes"
+                TextData = "C# Learning ",
+                Heading = "C-sharp ",
+                ShortNotes = "Tutorial On C-Sharp",
+                FeedTypeId = 2,
+                Link = "#",
+                UserName = "Avi"
+
             });
 
             feed.Add(new Feed
             {
                 Id = 3,
-                TextData = "FeedTypeId"
-            });
+                TextData = "UI Design",
+                Heading = "Web Design",
+                ShortNotes = "Learn Website design",
+                FeedTypeId = 3,
+                Link = "#",
+                UserName = "Anvi"
 
+            });
             feed.Add(new Feed
             {
                 Id = 4,
-                TextData = "Link"
+                TextData = ".Net Learning",
+                Heading = "Dot Net",
+                ShortNotes = "ASP .Net Learning Session",
+                FeedTypeId = 4,
+                Link = "#",
+                UserName = "ketaki"
+
             });
-            feed.Add(new Feed
+            return feed;
+        }
+        public IActionResult Index( string orderBy, string orderType, string IdSearch, string TextDataSearch)
+        {
+            List<Feed> feed = GetFeed();
+            if (!string.IsNullOrWhiteSpace(IdSearch))
             {
-                Id = 5,
-                TextData = "UserName"
-            });
-            feed.Add(new Feed
+                feed = feed.Where(x => x.Id.ToString() == IdSearch).ToList();
+            }
+            if (!string.IsNullOrWhiteSpace(TextDataSearch))
             {
-                Id = 6,
-                TextData = "FeedDate"
-            });
+                feed = feed.Where(x => x.TextData.Contains(TextDataSearch)).ToList();
+            }
+            if ("asc".Equals(orderBy, System.StringComparison.OrdinalIgnoreCase))
+            {
+                if ("Id".Equals(orderBy, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    feed = feed.OrderBy(x => x.Id).ToList();
+                }
+                else if ("TextData".Equals(orderBy, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    feed = feed.OrderBy(x => x.TextData).ToList();
+                }
+                else if ("Heading".Equals(orderBy, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    feed = feed.OrderBy(x => x.Heading).ToList();
+                }
+                else if ("ShortNotes".Equals(orderBy, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    feed = feed.OrderBy(x => x.ShortNotes).ToList();
+                }
+                else if ("Link".Equals(orderBy, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    feed = feed.OrderBy(x => x.Link).ToList();
+                }
+                else if ("UserName".Equals(orderBy, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    feed = feed.OrderBy(x => x.UserName).ToList();
+                }
+                orderType = "desc";
+            }
+            else
+            {
+                if ("Id".Equals(orderBy, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    feed = feed.OrderByDescending(x => x.Id).ToList();
+                }
+                else if ("TextData".Equals(orderBy, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    feed = feed.OrderByDescending(x => x.TextData).ToList();
+                }
+                else if ("Heading".Equals(orderBy, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    feed = feed.OrderByDescending(x => x.Heading).ToList();
+                }
+                else if ("ShortNotes".Equals(orderBy, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    feed = feed.OrderByDescending(x => x.ShortNotes).ToList();
+                }
+                else if ("Link".Equals(orderBy, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    feed = feed.OrderByDescending(x => x.Link).ToList();
+                }
+                else if ("UserName".Equals(orderBy, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    feed = feed.OrderByDescending(x => x.UserName).ToList();
+                }
+                orderType = "asc";
+            }
+            ViewBag.OrderType = orderType;
             return View(feed);
         }
     }
