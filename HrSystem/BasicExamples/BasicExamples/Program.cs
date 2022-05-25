17fbeb962f1ee3c68ec715fde6babcec478eb622
abhayvel <quickitdotnet@gmail.com>
2022-05-25 21:13:23 +0530

    added

--- HrSystem/BasicExamples/BasicExamples/Program.cs
+++ HrSystem/BasicExamples/BasicExamples/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Linq;
 
 namespace BasicExamples
 {
-    internal class Program
+    public class StartUp
     {
-        static void Main(string[] args)
+        public void Main()
         {
 
+        }
+
+        public void Call()
+        {
+
+        }
+
+        public void GetData()
+        {
+
+        }
+    }
+
+    internal class Program
+    {
+
+        public static void UseStartUp<T>() where T : new()
+        {
+            var startUpType=new T();
+
+            var methods = startUpType.GetType().GetMethods();
+          var  main= methods.Where(x => x.Name == "Main").FirstOrDefault();
+            if(main == null)
+            {
+                throw new Exception("Configure method is not part of Startup class.");
+            } else
+            {
+                main.Invoke(startUpType, null);
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                UseStartUp<StartUp>();
+            }
+            catch (Exception ex)
+            {
+
+            }
+           
             Console.WriteLine("Hello World!");
         }
     }
